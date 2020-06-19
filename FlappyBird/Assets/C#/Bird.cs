5a622b5f42e8ef7e9b4cd2d8ff1358867e8d25ad
st-dionysus <37880480+st-dionysus@users.noreply.github.com>
2020-06-19 14:02:24 +0300

    init

---
+++ FlappyBird/Assets/C#/Bird.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    public float force;                                 
+    Rigidbody2D BirdRigid;                              
+
+    public GameObject RestartButton;                    
+
+    void Start()
+    {
+        Time.timeScale = 1;                             
+        BirdRigid = GetComponent<Rigidbody2D>();        
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))                
+        {
+            BirdRigid.velocity = Vector2.up * force;    
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)  
+    {
+        if (collision.collider.tag == "Enemy")          
+        {
+            Destroy(gameObject);                        
+            Time.timeScale = 0;                         
+            RestartButton.SetActive(true);              
+        }
+    }
+
+    
+}
---
+++ FlappyBird/Assets/C#/PipeSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSpawner : MonoBehaviour
+{
+    public GameObject Pipes;        
+
+    void Start()
+    {
+        StartCoroutine(Spawner());  
+    }
+
+    IEnumerator Spawner()           
+    {
+        while (true)                
+        {
+            yield return new WaitForSeconds(2);     
+            float rand = Random.Range(-1f, 2f);     
+            GameObject newPipes = Instantiate(Pipes, new Vector3(2, rand, 0), Quaternion.identity);     
+            Destroy(newPipes, 10);  
+        }
+    }
+
+    
+    
+}
